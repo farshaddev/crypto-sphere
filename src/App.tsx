6a3128238de68d14a7060ebe4441340c5cdb7e03
farshaddev <farshad.development@gmail.com>
2024-09-11 22:56:11 +0330

    feat: react router configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import MarketsList from './pages/MarketsList';
+import React from "react";
+import MarketsList from "./pages/MarketsList";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <MarketsList />
-    </div>
-  );
-}
+const App: React.FC = () => {
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<MarketsList />} />
+				<Route path="/market/:id" element={<>MarketDetails</>} />
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
